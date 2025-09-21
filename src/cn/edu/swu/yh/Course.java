07dca59a0ada8613585b8d56bfc306ca241ae420
Deeeeer29 <deeryh529@outlook.com>
2025-09-21 09:20:59 +0800

    add Student class and Course class

---
+++ src/cn/edu/swu/yh/Course.java
@@ -0,0 +1,6 @@
+package cn.edu.swu.yh;
+public class Course {
+    private String courseId;
+    private String courseName;
+    private String teacher;
+}
---
+++ src/cn/edu/swu/yh/Student.java
@@ -0,0 +1,7 @@
+package cn.edu.swu.yh;
+public class Student {
+    private String id;
+    private String name;
+    private String gender;
+    private int age;
+}
